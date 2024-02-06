55801d362c984868ec3747665163cee6470e449c
Subrata <roojsubrata0@gmail.com>
2024-02-06 21:12:19 +0530

    changing in api url

--- assets/js/api.js
+++ assets/js/api.js
@@ -20,10 +20,10 @@ export const url = {
         return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon){
-         return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`
+         return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`
     },
     reverseGeo(lat, lon){
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5&units=metric`
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5&units=metric`
     },
 
     /*
@@ -31,7 +31,7 @@ export const url = {
     */
    
     geo(query){
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
 
 }
